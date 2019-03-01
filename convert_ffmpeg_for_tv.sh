2517ba85286d4bf288dda60ba15c6d0e58cb876d
Konrad Zdeb <konrad.zdeb@me.com>
2019-03-01 17:36:07 +0100

    Convert for TV
    Script location
    
    Signed-off-by: Konrad Zdeb <konrad.zdeb@me.com>

---
+++ convert_ffmpeg_for_tv.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+######################################################################
+# @author      : Konrad
+# @file        : convert_ffmpeg_for_tv
+# @created     : Friday Mar 01, 2019 16:07:53 CET
+# @license     : GPL-3.0
+#
+# @description : Run two pass conversion to convert file to tv
+######################################################################
+
+
+# Take input file variable
+input_file = 
